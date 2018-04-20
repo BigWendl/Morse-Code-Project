1886fd9bc051ef74dc47146ce5a7e8ad78d6a514
japoland <japoland@hotmail.com>
2018-04-20 16:58:01 -0500

    Update Decode_Morse.cpp

--- Decode_Morse.cpp
+++ Decode_Morse.cpp
@@ -1,10 +1,15 @@
 #include "Decode_Morse.h"
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
+	Decode_Morse M;
+	
+	M.create_morse_map();
+	//cout << "b: " <<  " " << M.Morse_Map['b'] << endl;
 
-system("pause");
+	system("pause");
 	return 0;
 }
