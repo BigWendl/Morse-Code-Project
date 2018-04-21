d149926f4d13d0eada05d2b0f3eab60b9552fc74
japoland <japoland@hotmail.com>
2018-04-20 19:56:03 -0500

    Update Decode_Morse.h

--- Decode_Morse.h
+++ Decode_Morse.h
@@ -1,5 +1,5 @@
-#ifndef DECODE_MORSE_H_
-#define DECODE_MORSE_H_
+#ifndef MORSE_H_
+#define MORSE_H_
 
 #include <string>
 #include <fstream>
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class Decode_Morse {
+class Morse_Code {
 
 private:
 	char letter;
@@ -18,6 +18,8 @@ public:
 	map<char, string>Morse_Map;//The last line in the technical requirements says "You may use a binary search tree or a map to store the codes for letters."
 	//I've emailed the instructor for clarification because it seemed easier to store the values in a map. 
 	//I made the map public for now because I was struggling with the getter. We can fix that and make it private later if needed.
+	//Reply from instructor said "The map or BST is intended for encoding whereas the Morse tree is intended for decoding. That means you still need
+	//to build the Morse tree and use it for decoding."
 		
 	void create_morse_map()//Function to populate the map with the letters and morse code equivalent.
 	{
@@ -28,15 +30,26 @@ public:
 		}
 	}
 
-	string decode_msg(string input)//Need a function that will accept an input string to convert FROM morse code
+	void create_morse_tree()//Function to populate a tree with letters and morse code equivalents
+	{
+
+
+	}
+
+	string decode_msg(string input)//Function accepts morse code input string to and returns result of conversion
 	{
 
 	}
 
-	string code_msg(string input)//Need a function that will accept an input string to convert TO morse code
+	string encode_msg(string input)//Function accepts input string and returns morse code result 
 	{
-
-
+		string result = "";//initialize result string to null
+		for (int i = 0; i < input.size(); i++)//for loop to go through each character in the input string
+		{
+			result += Morse_Map[input[i]] + " ";//add morse code result of each character to result string followed by a space
+		}
+		return result;//return result string
 	}
 };
 #endif
+
