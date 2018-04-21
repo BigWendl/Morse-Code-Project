40d09d4cdece230ef076fd17874122d55cad46f5
japoland <japoland@hotmail.com>
2018-04-20 19:59:17 -0500

    Update and rename Decode_Morse.cpp to Morse_Code.cpp

--- Morse_Code.cpp
+++ Morse_Code.cpp
@@ -1,11 +1,11 @@
-#include "Decode_Morse.h"
+#include "Morse_Code.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	Decode_Morse M;
+	Morse_Code M;
 	
 	M.create_morse_map();
 	cout << "computer = " << M.encode_msg("computer") << endl;
