f9fcb976f37d18856aa3a919e7fa1d17c9d91f1d
BigWendl <37221842+BigWendl@users.noreply.github.com>
2018-04-24 13:21:09 -0500

    Update Morse_Code.h

--- Morse_Code.h
+++ Morse_Code.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <map>
 #include <iostream>
+#include "Binary_Tree.h" 
 
 using namespace std;
 
@@ -13,7 +14,9 @@ class Morse_Code {
 private:
 	char letter;
 	string morse;
-
+	vector<string> codeVector{ " ",".","..","...","....","NULL","NULL","..._","NULL","NULL",".._",".._.","NULL","NULL","NULL","._","._.","._..","NULL","NULL","NULL",".__",".__.","NULL","NULL",".___","NULL","NULL","_","_.","_..","_...","NULL" ,"NULL","_.._","NULL" ,"NULL" ,"_._","_._.","NULL","NULL","_.__","NULL","NULL","__","__.","__..","NULL","NULL","__._","NULL","NULL","___","NULL","NULL" };
+	vector<string> letterVector{ " ","e","i","s","h","NULL","NULL","v","NULL","NULL","u","f","NULL","NULL","NULL","a","r","l","NULL","NULL","NULL","w","p","NULL","NULL","j","NULL","NULL","t","n","d","b","NULL" ,"NULL","x","NULL" ,"NULL" ,"k","c","NULL","NULL","y","NULL","NULL","m","g","z","NULL","NULL","q","NULL","NULL","o","NULL","NULL" };
+	
 public:
 	map<char, string>Morse_Map;//The last line in the technical requirements says "You may use a binary search tree or a map to store the codes for letters."
 	//I've emailed the instructor for clarification because it seemed easier to store the values in a map. 
@@ -29,16 +32,31 @@ public:
 			Morse_Map[letter] = morse;//set key to letter and value to the associated morse code
 		}
 	}
-
-	void create_morse_tree()//Function to populate a tree with letters and morse code equivalents
-	{
-
-
-	}
-
+	
 	string decode_msg(string input)//Function accepts morse code input string to and returns result of conversion
-	{
+	{		
+		string result = "", mCode;
+		Binary_Tree<string> letter;
+		letter.read_tree(letterVector);
+		Binary_Tree<string> code;
+		code.read_tree(codeVector);
 
+		istringstream tokens(input);
+
+		while (tokens >> mCode)
+		{
+			for (int i = 0; i < mCode.size(); i++)
+			{
+				if (mCode[i] == '.')
+					letter = letter.get_left_subtree();
+				if (mCode[i] == '_')
+					letter = letter.get_right_subtree();
+			}
+
+			result += letter.get_data();
+			letter.read_tree(letterVector);
+		}
+		return result;
 	}
 
 	string encode_msg(string input)//Function accepts input string and returns morse code result 
