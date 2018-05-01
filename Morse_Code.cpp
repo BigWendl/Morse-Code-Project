ae67af89d009ebaddf656a8d12c8e9127e6cbf9f
BigWendl <37221842+BigWendl@users.noreply.github.com>
2018-05-01 12:14:11 -0500

    Update Morse_Code.cpp

--- Morse_Code.cpp
+++ Morse_Code.cpp
@@ -7,9 +7,7 @@ int main()
 {
 	Morse_Code M;
 	
-	M.create_morse_map();
 	cout << "computer = " << M.encode_msg("computer") << endl;
-	
 	cout << ".__. ._. ___ __. ._. ._ __ decoded is: " << M.decode_msg(".__. ._. ___ __. ._. ._ __") << endl;
 
 	system("pause");
