03cac440026e30c881cb225906cf9f4580d726e7
BigWendl <37221842+BigWendl@users.noreply.github.com>
2018-05-03 17:11:47 -0500

    Update Morse_Code.cpp

--- Morse_Code.cpp
+++ Morse_Code.cpp
@@ -1,15 +1,17 @@
-#include <iostream>
+#include "stdafx.h"
 #include "Morse.h"
-
-using namespace std;
 
 int main()
 {
 	Morse_Code M;
-	
-	cout << "computer = " << M.encode_msg("computer") << endl;
+
+	cout << "computer coded is: " << M.encode_msg("computer") << endl;
 	cout << ".__. ._. ___ __. ._. ._ __ decoded is: " << M.decode_msg(".__. ._. ___ __. ._. ._ __") << endl;
 
+	cout << "apple coded is: " << M.encode_msg("apple") << endl;
+	cout << "_._. ._ _. _.. _.__ decoded is: " << M.decode_msg("_._. ._ _. _.. _.__") << endl;
+
+	cout << endl;
 	system("pause");
 	return 0;
 }
